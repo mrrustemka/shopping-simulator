92d48066bef8e1e5281dc14a03d9d8933d1d5fb1
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2024-01-27 16:37:22 +0400

    create-card-component

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import TotalItems from "./TotalItems";
+import Card from "./Card";
 
 function Basket() {
   return (
     <div>
       <h1>Basket</h1>
       <TotalItems />
+      <Card />
       <Link to="/">Continue Shopping</Link>
     </div>
   );
---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div>
+      Please enter your 16 digit credit/debit card number:
+      <input />
+    </div>
+  );
+}
+
+export default Card;
