6d1ce1c13ad44c65fca307744bf05f029c43a8a4
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2024-01-27 16:50:22 +0400

    create-basket-product-component

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom";
 import Total from "./Total";
 import Card from "./Card";
 import Checkout from "./Checkout";
+import BasketProduct from "./BasketProduct";
 
 function Basket() {
   return (
     <div>
       <h1>Basket</h1>
       <Total />
+      <BasketProduct />
       <Card />
       <Link to="/">Continue Shopping</Link>
       <Checkout />
---
+++ src/components/BasketProduct.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function BasketProduct() {
+  return (
+    <div className="basket">
+      <p>Name</p>
+      <p>Quantity</p>
+      <p>Unit Price</p>
+      <p>Total Price</p>
+      <button>Remove All</button>
+    </div>
+  );
+}
+
+export default BasketProduct;
