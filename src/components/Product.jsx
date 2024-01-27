14c41a38c90250be0e8c5ed0c321fd6fb315c573
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2024-01-27 16:46:29 +0400

    create-product-component

---
+++ src/components/Product.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Product() {
+  return (
+    <div className="product">
+      <p>Name</p>
+      <p>Price</p>
+      <button>Add to Basket</button>
+      <button>Remove from Basket</button>
+    </div>
+  );
+}
+
+export default Product;
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import TotalItems from "./Total";
+import Total from "./Total";
+import Product from "./Product";
 
 function ProductList() {
   return (
     <div>
       <h1>Product List</h1>
-      <TotalItems />
+      <Total />
+      <Total />
+      <Product />
       <Link to="/basket">Proceed to checkout</Link>
     </div>
   );
