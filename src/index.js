05a9d8e056a00950fd0a223f3ab10a2770550b9f
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2024-01-29 13:02:16 +0400

    fix-adding-product-to-basket

--- src/index.js
+++ src/index.js
@@ -9,24 +9,11 @@ import "./index.css";
 const defaultState = defaultBasket;
 
 const reducer = (state = defaultState, action) => {
-  console.log(state.basket);
-  // console.log(state.basket.filter((el) => Object.values(el)[0] === action.payload));
-  // console.log(state.basket.indexOf())
-  // console.log(state.basket.splice(0, 1));
-  let a = new Object({sku: 1, quantity: 1});
-  // a.sku = action.payload;
-  // a.quantity = 1;
-  console.log(a);
   switch (action.type) {
-    case "ADD-TO-BASKET":  
-    return { 
-        ...state,   
-        // basket: state.basket.push({
-        //   2: {
-        //     sku: action.payload,
-        //     quantity: 1,
-        //   },
-        // }),
+    case "ADD-TO-BASKET":
+      return {
+        ...state,
+        basket: state.basket.push({ sku: action.payload, quantity: 1 }),
       };
     case "REMOVE-FROM-BASKET":
       return {
