bebf5570a62170b7bafd3c7bb9eab8349240e58a
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2024-01-31 13:44:14 +0400

    add-total-ptice-to-basket-list-component

--- src/components/BasketList.jsx
+++ src/components/BasketList.jsx
@@ -5,14 +5,20 @@ import Card from "./Card";
 import Checkout from "./Checkout";
 import BasketProduct from "./Basket";
 import { useSelector } from "react-redux";
+import products from "../data/products_sample.json";
 
 function Basket() {
-  const data = useSelector((state) => state.basket);
+  const data = useSelector((state) => state);
+  let basketPrice = 0;
+  data.basket.forEach((element) => {
+    const positionPrice = products.find((el) => el.sku === element.sku);
+    basketPrice += positionPrice.price * element.quantity;
+  });
   return (
     <div>
       <h1>Basket</h1>
-      <Info message="Total Price" count={0} />
-      {data.map((product) => (
+      <Info message="Total Price" count={basketPrice} />
+      {data.basket.map((product) => (
         <BasketProduct
           id={product.sku}
           quantity={product.quantity}
