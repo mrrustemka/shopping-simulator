607ca08e2cbfb7c3ddc98659e3397cd3124bf205
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2024-02-01 09:17:07 +0400

    complete-to-show-baslet-data

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { useDispatch } from "react-redux";
+import products from "../data/products_sample.json";
 
 function BasketProduct({ id, quantity }) {
   const dispatch = useDispatch();
-  // const customerInfo = useSelector((state) => state.basket);
 
   function removeBasket(id) {
     dispatch({ type: "REMOVE-FROM-BASKET", payload: id });
+  }
+
+  function getProductInfo(id) {
+    return products.find((element) => element.sku === id);
   }
 
   return (
     <div className="basket">
       <p>{id} | </p>
       <p>{quantity} | </p>
-      <p>Unit Price</p>
-      <p>Total Price</p>
+      <p>{getProductInfo(id)?.price} | </p>
+      <p>{getProductInfo(id)?.price * quantity} | </p>
       <button onClick={() => removeBasket(id)}>Remove All</button>
     </div>
   );
