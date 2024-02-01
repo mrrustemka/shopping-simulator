af765fcbc6fb7ffa3bbd887133dbd46d49270721
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2024-02-01 11:20:01 +0400

    create-product-state

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 function Product({ name, description, price, basketLimit, id }) {
   const dispatch = useDispatch();
-  // const customerInfo = useSelector((state) => state.basket);
+  const customerInfo = useSelector((state) => state);
+  console.log(customerInfo);
 
   const addBasket = (id) => {
     dispatch({ type: "ADD-TO-BASKET", payload: id });
@@ -12,13 +13,21 @@ function Product({ name, description, price, basketLimit, id }) {
     dispatch({ type: "REMOVE-FROM-BASKET", payload: id });
   }
 
+  const productState = (id) => {
+    console.log(customerInfo.basket.find((el) => el.sku === id));
+    return customerInfo.basket.find((el) => el.sku === id);
+  };
+
   return (
     <div className="product">
       <p>{name} | </p>
       <p>{description} | </p>
       <p>{price} | </p>
-      <button onClick={() => addBasket(id)}>Add to Basket</button>
-      <button onClick={() => removeBasket(id)}>Remove from Basket</button>
+      {productState(id) ? (
+        <button onClick={() => removeBasket(id)}>Remove from Basket</button>
+      ) : (
+        <button onClick={() => addBasket(id)}>Add to Basket</button>
+      )}
     </div>
   );
 }
