5b9d55616b22c9bd4e47b7d25534521fe73c6d62
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2024-02-02 14:13:09 +0400

    make-validation

--- src/components/BasketList.jsx
+++ src/components/BasketList.jsx
@@ -1,8 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Info from "./Info";
-import Card from "./Card";
-import Checkout from "./Checkout";
 import BasketProduct from "./Basket";
 import { useSelector } from "react-redux";
 import products from "../data/products_sample.json";
@@ -10,24 +8,43 @@ import products from "../data/products_sample.json";
 function Basket() {
   const customerInfo = useSelector((state) => state);
   let basketPrice = 0;
+  const [cardNumber, setCardNumber] = useState("");
+
   customerInfo.basket.forEach((element) => {
     const positionPrice = products.find((el) => el.sku === element.sku);
     basketPrice += positionPrice.price * element.quantity;
   });
+
+  function isValidCardNumber() {
+    if (cardNumber.length !== 16) {
+      alert("Incorrect Card Number");
+    }
+  }
+
   return (
     <div>
-      <h1>Basket</h1>
-      <Info message="Total Price" count={basketPrice} />
-      {customerInfo.basket.map((product) => (
-        <BasketProduct
-          id={product.sku}
-          quantity={product.quantity}
-          key={product.sku}
+      <form>
+        <h1>Basket</h1>
+        <Info message="Total Price" count={basketPrice} />
+        {customerInfo.basket.map((product) => (
+          <BasketProduct
+            id={product.sku}
+            quantity={product.quantity}
+            key={product.sku}
+          />
+        ))}
+        Bank Card:
+        <input
+          id="card"
+          placeholder="Enter your credit/debit card..."
+          type="text"
+          onChange={(e) => setCardNumber(e.target.value)}
         />
-      ))}
-      <Card />
+        <button type="submit" onClick={isValidCardNumber}>
+          Checkout
+        </button>
+      </form>
       <Link to="/">Continue Shopping</Link>
-      <Checkout />
     </div>
   );
 }
